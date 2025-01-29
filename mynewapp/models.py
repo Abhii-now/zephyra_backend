e05c5c41db948a82e93f021155c6c399daad1172
Abhinav Jain <jainabhinav2k16@gmail.com>
2025-01-29 17:44:09 +0530

    feat: Implement role-based and permission-based authentication
    - Added role-based and permission-based authentication to the project
    - Updated validator.py to include role and permission checks
    - Enhanced security by ensuring only authorized users can access specific endpoints
    - Updated models to store user roles and permissions

--- mynewapp/models.py
+++ mynewapp/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 #
@@ -9,6 +10,7 @@ class MyModel(models.Model):
         return self.name
 class EncryptedFile(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    size= models.IntegerField(default=0)
     iv = models.BinaryField()
     tag = models.BinaryField()
     encrypted_data = models.BinaryField()
@@ -18,3 +20,13 @@ class EncryptedFile(models.Model):
     
     def __str__(self):
         return self.name
+    
+class SharableLink(models.Model):
+    file = models.ForeignKey(EncryptedFile, to_field='name', on_delete=models.CASCADE)
+    token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    expiration = models.DateTimeField()
+    link_type = models.CharField(max_length=255, default='view')
+    user_ids = models.JSONField(default=list)  # Add this line to store array of user IDs
+
+    def __str__(self):
+        return self.file.name
